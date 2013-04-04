c9098e342ffb7b585ffd430f985ad676a5421efc
Goran Blazin <goran.blazin@gmai.com>
2013-04-04 15:57:44 +0200

    task #2731: Dodati install linkove za extenzije koje fale u probe.php

--- probe.php
+++ probe.php
@@ -252,11 +252,11 @@
     } // if
     
     $recommended_extensions = array(
-      'gd' => 'GD is used for image manipulation. Without it, system is not able to create thumbnails for files or manage avatars, logos and project icons', 
-      'mbstring' => 'MultiByte String is used for work with Unicode. Without it, system may not split words and string properly and you can have weird question mark characters in Recent Activities for example', 
-      'curl' => 'cURL is used to support various network tasks', 
-      'iconv' => 'Iconv is used for character set conversion. Without it, system is a bit slower when converting different character set', 
-      'imap' => 'IMAP is used to connect to POP3 and IMAP servers. Without it, Incoming Mail module will not work',
+      'gd' => 'GD is used for image manipulation. Without it, system is not able to create thumbnails for files or manage avatars, logos and project icons. Please refer to <a href="http://www.php.net/manual/en/image.installation.php">this</a> page for installation instructions', 
+      'mbstring' => 'MultiByte String is used for work with Unicode. Without it, system may not split words and string properly and you can have weird question mark characters in Recent Activities for example. Please refer to <a href="http://www.php.net/manual/en/mbstring.installation.php">this</a> page for installation instructions', 
+      'curl' => 'cURL is used to support various network tasks. Please refer to <a href="http://www.php.net/manual/en/curl.installation.php">this</a> page for installation instructions', 
+      'iconv' => 'Iconv is used for character set conversion. Without it, system is a bit slower when converting different character set. Please refer to <a href="http://www.php.net/manual/en/iconv.installation.php">this</a> page for installation instructions', 
+      'imap' => 'IMAP is used to connect to POP3 and IMAP servers. Without it, Incoming Mail module will not work. Please refer to <a href="http://www.php.net/manual/en/imap.installation.php">this</a> page for installation instructions', 
       // SVN extension ommited, to avoid confusion
     );
 
